62f80fdcf72dd72436bc4420c4027ea3a19a5234
Astrian Zheng <happy.wheel6743@astrian.moe>
2024-09-06 19:11:12 +1000

    Add instanceable feature Now user can create a new SAN instance and send the request to a unique backend server

--- Sources/SwiftAsyncNetwork/SwiftAsyncNetwork.swift
+++ Sources/SwiftAsyncNetwork/SwiftAsyncNetwork.swift
@@ -1,10 +1,40 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Astrian Zheng on 2022/4/27.
 //
 
 import Foundation
+import SwiftUI
 
 public let SAN = Session.default
+public class SANInstance: ObservableObject {
+  static let `default` = Session()
+  
+  // Input base URL
+  public init(baseURL: String) {
+    // Detect the URL is valid
+    guard let _ = URL(string: baseURL) else {
+      fatalError("Invalid URL")
+    }
+    // remove the last slash
+    if baseURL.last == "/" {
+      self.baseURL = String(baseURL.dropLast())
+    } else {
+      self.baseURL = baseURL
+    }
+  }
+  
+  // Request function, calling Session.request
+  public func request(_ method: String = "GET", _ path: String, params: SANReqParams? = nil) async throws -> (Data, HTTPURLResponse?) {
+    // if the first character of path is not "/", add it
+    var path = path
+    if path.first != "/" {
+      path = "/\(path)"
+    }
+    return try await Session.default.request(method, baseURL + path, params: params)
+  }
+  
+  var baseURL: String
+}
